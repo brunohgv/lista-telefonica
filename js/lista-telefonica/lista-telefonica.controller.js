93b2a0e3f10b8a9ccc96ffc8a748076fdbf3187f
Bruno Gusmão <brunohgv2@gmail.com>
2018-11-22 21:57:13 -0300

    Operadora adicionado

--- js/lista-telefonica/lista-telefonica.controller.js
+++ js/lista-telefonica/lista-telefonica.controller.js
@@ -10,6 +10,13 @@ angular
     {nome: "Björn", telefone: "98698745"},
   ]
 
+  $scope.operadoras = [
+    {nome: "Oi", codigo: 31},
+    {nome: "Vivo", codigo: 15},
+    {nome: "Tim", codigo: 41},
+    {nome: "Claro", codigo: 21},
+  ]
+
   $scope.adicionarContato = function (contato) {
     $scope.contatos.push(angular.copy(contato))
     delete $scope.contato
