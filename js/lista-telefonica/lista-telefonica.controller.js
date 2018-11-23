9f75506739d0e917a88dea268d3fba88fcda2a9f
Bruno Gusmão <brunohgv2@gmail.com>
2018-11-22 22:21:08 -0300

    Deletando contatos

--- js/lista-telefonica/lista-telefonica.controller.js
+++ js/lista-telefonica/lista-telefonica.controller.js
@@ -11,15 +11,26 @@ angular
   ]
 
   $scope.operadoras = [
-    {nome: "Oi", codigo: 31},
-    {nome: "Vivo", codigo: 15},
-    {nome: "Tim", codigo: 41},
-    {nome: "Claro", codigo: 21},
+    {nome: "Oi", codigo: 31, categoria: "Celular"},
+    {nome: "Vivo", codigo: 15, categoria: "Celular"},
+    {nome: "Tim", codigo: 41, categoria: "Celular"},
+    {nome: "Claro", codigo: 21, categoria: "Celular"},
+    {nome: "GVT", codigo: 15, categoria: "Fixo"},
+    {nome: "NET", codigo: 21, categoria: "Fixo"},
   ]
+
+  $scope.classeSelecionado = "selecionado"
 
   $scope.adicionarContato = function (contato) {
     $scope.contatos.push(angular.copy(contato))
     delete $scope.contato
+  }
+
+  $scope.deletarContato = function (contatos) {
+    $scope.contatos = contatos.filter(contato => {
+      if (!contato.selecionado)
+        return contato
+    })
   }
 
 })
