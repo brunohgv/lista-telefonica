a561be3fd44854f470d15d9ceea072ae845450a7
Bruno Gusmão <brunohgv2@gmail.com>
2018-11-23 02:42:32 -0300

    Factory para contato

---
+++ js/lista-telefonica/contato/contato.factory.js
@@ -0,0 +1,16 @@
+angular
+.module("listaTelefonica")
+.factory("contatosAPI", function ($http) {
+  var _getContatos = function () {
+    return $http.get("http://localhost:3412/contatos")
+  }
+
+  var _salvarContato = function (contato) {
+    return $http.post("http://localhost:3412/contatos", contato)
+  }
+
+  return {
+    getContatos: _getContatos,
+    salvarContato: _salvarContato
+  }
+})
