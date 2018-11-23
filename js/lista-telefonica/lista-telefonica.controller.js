ad75c9354f0d69aaa29e345882fbe16b4a049d30
Bruno Gusmão <brunohgv2@gmail.com>
2018-11-22 21:28:27 -0300

    Lista telefones completa

--- js/lista-telefonica/lista-telefonica.controller.js
+++ js/lista-telefonica/lista-telefonica.controller.js
@@ -1,5 +1,12 @@
 angular
 .module("listaTelefonica")
 .controller("listaTelefonicaCtrl", function ($scope) {
-  $scope.titulo = "Meu título"
+  $scope.titulo = "Lista Telefônica"
+
+  $scope.contatos = [
+    {nome: "Bruno", telefone: "99997777"},
+    {nome: "Adriana", telefone: "98984545"},
+    {nome: "Adalberto", telefone: "99887878"},
+    {nome: "Björn", telefone: "98698745"},
+  ]
 })
