d1cde9fcc472e36723e4bd73b064c2d70bdead59
Bruno Gusmão <brunohgv2@gmail.com>
2018-11-24 18:16:04 -0300

    Interceptors

---
+++ app/interceptors/error-interceptor.js
@@ -0,0 +1,12 @@
+angular
+.module("listaTelefonica")
+.factory("errorInterceptor", function ($q, $location) {
+  return {
+    responseError: function (rejection) {
+      if (rejection.status === 404) {
+        $location.path('/erro')
+      }
+      return $q.reject(rejection)
+    }
+  }
+})
