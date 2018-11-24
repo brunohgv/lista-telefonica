e980d970cfd198eafc1effd546f8f78b50d44376
Bruno Gusmão <brunohgv2@gmail.com>
2018-11-24 15:59:56 -0300

    Fluxo de navegação

--- js/lista-telefonica/novo-contato/novo-contato.controller.js
+++ js/lista-telefonica/novo-contato/novo-contato.controller.js
@@ -1,19 +1,32 @@
 angular
 .module("listaTelefonica")
-.controller("novoContatoCtrl", function ($scope, contatosAPI, serialGenerator) {
+.controller("novoContatoCtrl", function ($scope, $location, contatosAPI, operadorasAPI, serialGenerator) {
 
   $scope.adicionarContato = function (contato) {
     contato.serial = serialGenerator.generate()
     contato.data = new Date()
     contatosAPI.salvarContato(contato)
       .then(() => {
-        carregarContatos()
         delete $scope.contato
         $scope.contatoForm.$setPristine()
+        $location.path("/contatos")
       })
       .catch(err => {
         $scope.erro = err.status + ": " + err.statusText
       })
   }
+
+  const carregarOperadoras = function () {
+    operadorasAPI.getOperadoras()
+      .then(response => {
+        $scope.operadoras = response.data
+      })
+      .catch(err => {
+        // alert("Erro " + error.status + ": " + error.statusText)
+        $scope.erroOperadoras = err.status + ": " + err.statusText
+      })
+  }
+
+  carregarOperadoras()
 
 })
